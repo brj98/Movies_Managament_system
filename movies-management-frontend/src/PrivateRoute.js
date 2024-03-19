97ec828f3a42dc2f433a747b2b17fa15212a8efe
brj98 <101431078+brj98@users.noreply.github.com>
2024-03-19 20:04:29 +0300

    Delete movies-management-frontend/src/PrivateRoute.js

--- movies-management-frontend/src/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ element, authenticated }) => {
-  return authenticated ? element : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
